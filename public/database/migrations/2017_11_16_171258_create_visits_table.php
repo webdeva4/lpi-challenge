6bb88e2ab3bd918a86089193358ff427377c1545
Laurie Perry <laurie@lauriemperry.com>
2017-11-16 21:56:35 -0600

    initial project commit, Create new Lumen project, database and migrations

---
+++ public/database/migrations/2017_11_16_171258_create_visits_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVisitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // create cities table
+        Schema::create('visits', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('city_id')->unsigned();
+            $table->foreign('city_id')->references('id')->on('cities');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //drop cities table
+        Schema::dropIfExists('visits');
+    }
+}
