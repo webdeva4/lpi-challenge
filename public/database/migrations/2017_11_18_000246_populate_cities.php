6edfbaff914551fd000388154dca9508563d31bb
Laurie Perry <laurie@lauriemperry.com>
2017-11-19 15:25:02 -0600

    populate the cities table with test data

--- public/database/migrations/2017_11_18_000246_populate_cities.php
+++ public/database/migrations/2017_11_18_000246_populate_cities.php
@@ -13,96 +13,96 @@ class PopulateCities extends Migration
      */
     public function up()
     {
-//        DB::table('cities')->insert([
-//                'city' => 'Milwaukee',
-//                'state' =>'WI',
-//                'created_at' => date('Y-m-d H:i:s'),
-//                'updated_at' => date('Y-m-d H:i:s')]
-//        );
-//        DB::table('cities')->insert([
-//                'city' => 'New Berlin',
-//                'state' =>'WI',
-//                'created_at' => date('Y-m-d H:i:s'),
-//                'updated_at' => date('Y-m-d H:i:s')]
-//        );
-//        DB::table('cities')->insert([
-//                'city' => 'Hartford',
-//                'state' =>'WI',
-//                'created_at' => date('Y-m-d H:i:s'),
-//                'updated_at' => date('Y-m-d H:i:s')]
-//        );
-//        DB::table('cities')->insert([
-//                'city' => 'Madison',
-//                'state' =>'WI',
-//                'created_at' => date('Y-m-d H:i:s'),
-//                'updated_at' => date('Y-m-d H:i:s')]
-//        );
-//        DB::table('cities')->insert([
-//                'city' => 'San Diego',
-//                'state' =>'CA',
-//                'created_at' => date('Y-m-d H:i:s'),
-//                'updated_at' => date('Y-m-d H:i:s')]
-//        );
-//        DB::table('cities')->insert([
-//                'city' => 'San Francisco',
-//                'state' =>'CA',
-//                'created_at' => date('Y-m-d H:i:s'),
-//                'updated_at' => date('Y-m-d H:i:s')]
-//        );
-//        DB::table('cities')->insert([
-//                'city' => 'Oakland',
-//                'state' =>'CA',
-//                'created_at' => date('Y-m-d H:i:s'),
-//                'updated_at' => date('Y-m-d H:i:s')]
-//        );
-//        DB::table('cities')->insert([
-//                'city' => 'Portland',
-//                'state' =>'OR',
-//                'created_at' => date('Y-m-d H:i:s'),
-//                'updated_at' => date('Y-m-d H:i:s')]
-//        );
-//        DB::table('cities')->insert([
-//                'city' => 'Eugene',
-//                'state' =>'OR',
-//                'created_at' => date('Y-m-d H:i:s'),
-//                'updated_at' => date('Y-m-d H:i:s')]
-//        );
-//        DB::table('cities')->insert([
-//                'city' => 'Phoenix',
-//                'state' =>'AZ',
-//                'created_at' => date('Y-m-d H:i:s'),
-//                'updated_at' => date('Y-m-d H:i:s')]
-//        );
-//        DB::table('cities')->insert([
-//                'city' => 'Sedona',
-//                'state' =>'AZ',
-//                'created_at' => date('Y-m-d H:i:s'),
-//                'updated_at' => date('Y-m-d H:i:s')]
-//        );
-//        DB::table('cities')->insert([
-//                'city' => 'Tucsan',
-//                'state' =>'AZ',
-//                'created_at' => date('Y-m-d H:i:s'),
-//                'updated_at' => date('Y-m-d H:i:s')]
-//        );
-//        DB::table('cities')->insert([
-//                'city' => 'Arlington',
-//                'state' =>'IL',
-//                'created_at' => date('Y-m-d H:i:s'),
-//                'updated_at' => date('Y-m-d H:i:s')]
-//        );
-//        DB::table('cities')->insert([
-//                'city' => 'Chicago',
-//                'state' =>'IL',
-//                'created_at' => date('Y-m-d H:i:s'),
-//                'updated_at' => date('Y-m-d H:i:s')]
-//        );
-//        DB::table('cities')->insert([
-//                'city' => 'Naperville',
-//                'state' =>'IL',
-//                'created_at' => date('Y-m-d H:i:s'),
-//                'updated_at' => date('Y-m-d H:i:s')]
-//        );
+        DB::table('cities')->insert([
+                'city' => 'Milwaukee',
+                'state' =>'WI',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')]
+        );
+        DB::table('cities')->insert([
+                'city' => 'New Berlin',
+                'state' =>'WI',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')]
+        );
+        DB::table('cities')->insert([
+                'city' => 'Hartford',
+                'state' =>'WI',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')]
+        );
+        DB::table('cities')->insert([
+                'city' => 'Madison',
+                'state' =>'WI',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')]
+        );
+        DB::table('cities')->insert([
+                'city' => 'San Diego',
+                'state' =>'CA',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')]
+        );
+        DB::table('cities')->insert([
+                'city' => 'San Francisco',
+                'state' =>'CA',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')]
+        );
+        DB::table('cities')->insert([
+                'city' => 'Oakland',
+                'state' =>'CA',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')]
+        );
+        DB::table('cities')->insert([
+                'city' => 'Portland',
+                'state' =>'OR',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')]
+        );
+        DB::table('cities')->insert([
+                'city' => 'Eugene',
+                'state' =>'OR',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')]
+        );
+        DB::table('cities')->insert([
+                'city' => 'Phoenix',
+                'state' =>'AZ',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')]
+        );
+        DB::table('cities')->insert([
+                'city' => 'Sedona',
+                'state' =>'AZ',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')]
+        );
+        DB::table('cities')->insert([
+                'city' => 'Tucsan',
+                'state' =>'AZ',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')]
+        );
+        DB::table('cities')->insert([
+                'city' => 'Arlington',
+                'state' =>'IL',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')]
+        );
+        DB::table('cities')->insert([
+                'city' => 'Chicago',
+                'state' =>'IL',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')]
+        );
+        DB::table('cities')->insert([
+                'city' => 'Naperville',
+                'state' =>'IL',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')]
+        );
     }
 
     /**
