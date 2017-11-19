8d2ca8d857f3ab850f086ab47f30ea336ee91249
Laurie Perry <laurie@lauriemperry.com>
2017-11-19 15:29:27 -0600

    Create restful routes for api

--- public/routes/web.php
+++ public/routes/web.php
@@ -14,3 +14,18 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+// List all cities in a state
+Route::get('/state/{state}/cities', 'VisitsController@listCities');
+
+//Return a list of cities the user has visited
+Route::get('/user/{user}/visits','VisitsController@cityVisits');
+
+//Return a list of states the user has visited
+Route::get('/user/{user}/visits/states', 'VisitsController@stateVisits');
+
+// Allow creating a new visit to a city
+Route::post('/user/{user}/visits', 'VisitsController@create');
+
+//Allow a user to remove a visit
+Route::delete('/visit/{visit}', 'VisitsController@remove');
