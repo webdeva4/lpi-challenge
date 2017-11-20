7bd559d7c5bf69fcaa187b7d3e1e29b6ba77d0f0
Laurie Perry <laurie@lauriemperry.com>
2017-11-19 21:10:05 -0600

    Move routes in order of challenge sheet

--- public/routes/web.php
+++ public/routes/web.php
@@ -18,14 +18,15 @@ $router->get('/', function () use ($router) {
 // List all cities in a state
 Route::get('/state/{state}/cities', 'VisitsController@listCities');
 
+// Allow creating a new visit to a city
+Route::post('/user/{user}/visits', 'VisitsController@create');
+
+//Allow a user to remove a visit
+Route::delete('/visit/{visit}', 'VisitsController@remove');
+
 //Return a list of cities the user has visited
 Route::get('/user/{user}/visits','VisitsController@cityVisits');
 
 //Return a list of states the user has visited
 Route::get('/user/{user}/visits/states', 'VisitsController@stateVisits');
 
-// Allow creating a new visit to a city
-Route::post('/user/{user}/visits', 'VisitsController@create');
-
-//Allow a user to remove a visit
-Route::delete('/visit/{visit}', 'VisitsController@remove');
